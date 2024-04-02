ff211d60a2ce5cee63b48e5295162927c75e4de0
Aswiniaddepalli <aswini1472002@gmail.com>
2024-04-02 14:30:31 +0530

    ExceptExcel

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -6,8 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 		features="features",
 		glue="stepDefinitions",
 		//plugin="com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		plugin={"pretty","html:target/CucumberReports.html","json:target/CucumberReport/JSONReport.json"},
-		dryRun=true)
+		plugin={"pretty","html:target/CucumberReports.html",
+				"json:target/CucumberReport/JSONReport.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		dryRun=true,
+		monochrome=true)
 public class RunnerClass extends AbstractTestNGCucumberTests{
 	
  }
